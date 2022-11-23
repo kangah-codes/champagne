5ceb7e45aaf581198ce0c128f1b1f0321dac33b8
Joshua Akangah <akangah89@gmail.com>
2022-11-23 23:08:30 +0000

    updates

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -225,7 +225,7 @@ const DataForm = () => {
 
 	return (
 		<div
-			className={`w-full p-5 xl:p-10 grid grid-cols-1 md:grid-cols-2 mx-auto max-w-7xl gap-10 items-center`}
+			className={`w-full py-5 xl:py-10 grid grid-cols-1 md:grid-cols-2 mx-auto items-center  gap-x-[100px]`}
 		>
 			<div className="flex flex-col justify-between">
 				<img
@@ -364,30 +364,32 @@ const SuccessForm = () => {
 	];
 
 	return (
-		<div className="flex flex-col lg1:flex-row p-5 lg1:p-14 w-full gap-x-5">
-			<div className="flex flex-col w-full lg1:w-1/2 my-auto">
+		<div
+			className={`w-full py-[34px] grid grid-cols-1 md:grid-cols-2 mx-auto items-center lg1:gap-x-[55px] gap-x-[100px]`}
+		>
+			<div className="flex flex-col w-full my-auto">
 				<img
-					className="w-[142px] xl:w-[252px] cursor-pointer mx-auto xl:mx-0"
+					className="w-[142px] xl:w-[175px] 2xl:w-[254px] 3xl:w-[336px] cursor-pointer mx-auto lg1:mx-0"
 					src="/images/logo.png"
 					alt="Champagne Logo"
 				/>
 
-				<h1 className="text-[47px] xl:text-[52px] font-anton leading-[47px] xl:leading-[52px] text-white text-center lg1:text-left">
+				<h1 className="text-[38px] leading-[38px] text-center lg1:text-left xl:text-[41px] xl:leading-[41px] 2xl:text-[58px] 2xl:leading-[58px] 3xl:text-[77px] 3xl:leading-[77px] font-anton text-white">
 					Congratulations
 					<br /> for joining the
 					<br /> Champagne waitlist.
 					<br /> We will keep you abreast of progress!
 				</h1>
 
-				<p className="text-white font-bold text-[12px] xl:text-[23px] leading-[12px] xl:leading-[23px] mt-5 text-center xl:text-left">
+				<p className="text-white font-bold text-[15px] leading-[15px] xl:text-[16px] xl:leading-[16px] 2xl:text-[23px] 2xl:leading-[23px] 3xl:text-[30px] 3xl:leading-[30px] mt-5 text-center lg1:text-left">
 					Invite your friends to win a bottle of Champagne. The more
 					of your friends join, the better your chances
 				</p>
 			</div>
 
-			<div className="flex flex-col items-center relative space-y-5 mt-8 xl:my-0">
-				<div className="w-[90%] md:w-[60%] xl:w-[386px] h-[278px] 2x:h-[409px] p-5 rounded-[14px] bg-champagne-yellow absolute -top-0.5 xl:-top-2 z-1" />
-				<div className="w-full md:w-[70%] xl:w-[404px] h-[278px] xl:h-[409px] p-5 rounded-[14px] bg-white flex flex-col justify-between z-10">
+			<div className="flex flex-col items-center relative space-y-5 xl:my-0">
+				<div className="w-[90%] md:w-[60%] lg1:w-[257px] lg1:h-[275px] 2xl:w-[386px] 3xl:w-[515px] 3xl:h-[551px] rounded-[14px] bg-champagne-yellow absolute -top-0.5 xl:-top-1 z-1" />
+				<div className="w-full md:w-[70%] lg1:w-[269px] lg1:h-[273px] 2xl:h-[409px] 2xl:w-[404px] 3xl:w-[539px] 3xl:h-[546px] p-[12px] 2xl:p-[26px] 3xl:p-[34px] rounded-[14px] bg-white flex flex-col justify-between z-10">
 					<div className="flex flex-row justify-between w-full">
 						<div className="flex flex-row space-x-2">
 							<button
@@ -398,11 +400,10 @@ const SuccessForm = () => {
 										)}`
 									);
 								}}
-								className="bg-champagne-lighter-gray rounded-full flex flex-row items-center justify-center py-2 px-5 text-base font-black"
+								className="bg-champagne-lighter-gray rounded-full flex flex-row items-center justify-center py-2 pl-[12px] pr-[17px] text-base font-black"
 							>
-								<LinkIcon className="text-champagne-light-gray w-5 h-5" />
-								<p className="text-black text-[10px] leading-[10px] xl:text-[16px] font-anton xl:leading-[16px]">
-									Copy Card Link
+								<p className="text-black text-[10px] leading-[10px] 2xl:text-[16px] 2xl:leading-[16px] font-anton">
+									🔗 Copy Card Link
 								</p>
 							</button>
 							<div
@@ -413,10 +414,10 @@ const SuccessForm = () => {
 										setDiceIndex(diceIndex + 1);
 									}
 								}}
-								className="cursor-pointer bg-champagne-lighter-gray rounded-full flex flex-row items-center justify-center py-2 px-5 text-base font-black"
+								className="cursor-pointer bg-champagne-lighter-gray rounded-full flex flex-row items-center justify-center py-2 px-[5px] 2xl:px-[12px] text-base font-black"
 							>
 								<img
-									className="w-[15px] h-[15px] xl:w-[28px] xl:h-[28px] cursor-pointer"
+									className="w-[15px] h-[15px] 2xl:w-[28px] 2xl:h-[28px] cursor-pointer"
 									src="/images/emojis/dice.png"
 									alt="Champagne Logo"
 								/>
@@ -424,28 +425,144 @@ const SuccessForm = () => {
 						</div>
 
 						<img
-							className="w-[80px] xl:w-[103px] cursor-pointer"
+							className="lg1:w-[68px] w-[80px] xl:w-[68px] 2xl:w-[104px] cursor-pointer"
 							src="/images/logo.png"
 							alt="Champagne Logo"
 						/>
 					</div>
 
 					<div>
-						<h1 className="text-[29px] leading-[29px] xl:text-[44px] font-anton xl:leading-[44px] text-black line-clamp-5">
+						<h1 className="text-[29px] leading-[29px] xl:text-[31px] xl:leading-[31px] 2xl:text-[44px] 2xl:leading-[44px] font-anton text-black line-clamp-5 3xl:line-clamp-[7] mt-10 lg1:mt-0">
 							{diceOptions[diceIndex]}
 						</h1>
 					</div>
 				</div>
 
 				<div className="max-w-sm">
-					<p className="text-black font-bold text-[14px] leading-[14px] text-center">
+					<p className="text-black font-bold text-[9px] leading-[9px] xl:text-[14px] xl:leading-[14px] 3xl:text-[18px] 3xl:leading-[18px] text-center">
 						Share our this card on your social or send this card to
 						a friend In your contact. You may win a bottle of
 						BeSocial Champagne
 					</p>
 				</div>
 
-				<div className="flex flex-row gap-x-2 w-full xl:max-w-[90%] 2xl:max-w-[97%] justify-between lg:gap-x-3 items-stretch">
+				<div className="grid grid-cols-7 gap-2 max-w-lg justify-between lg:gap-x-3 items-stretch">
+					<div
+						onClick={() => {
+							setInstaModal(true);
+						}}
+						className="cursor-pointer col-span-2 bg-black text-white rounded-full flex flex-row space-x-2 items-center justify-center py-1 px-2 lg:py-[11px] xl:py-[12px] lg:px-[15px] 2xl:py-[17px] text-base font-black"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.145"
+							height="17.139"
+							viewBox="0 0 17.145 17.139"
+						>
+							<path
+								id="Icon_awesome-instagram"
+								data-name="Icon awesome-instagram"
+								d="M8.568,6.413a4.394,4.394,0,1,0,4.394,4.394A4.387,4.387,0,0,0,8.568,6.413Zm0,7.251a2.857,2.857,0,1,1,2.857-2.857A2.862,2.862,0,0,1,8.568,13.664Zm5.6-7.431a1.025,1.025,0,1,1-1.025-1.025A1.023,1.023,0,0,1,14.167,6.233Zm2.91,1.04a5.072,5.072,0,0,0-1.384-3.591A5.106,5.106,0,0,0,12.1,2.3c-1.415-.08-5.656-.08-7.071,0A5.1,5.1,0,0,0,1.439,3.679,5.089,5.089,0,0,0,.055,7.27c-.08,1.415-.08,5.656,0,7.071a5.072,5.072,0,0,0,1.384,3.591,5.112,5.112,0,0,0,3.591,1.384c1.415.08,5.656.08,7.071,0a5.072,5.072,0,0,0,3.591-1.384,5.106,5.106,0,0,0,1.384-3.591c.08-1.415.08-5.653,0-7.068ZM15.25,15.86a2.892,2.892,0,0,1-1.629,1.629c-1.128.447-3.805.344-5.052.344s-3.928.1-5.052-.344A2.892,2.892,0,0,1,1.887,15.86c-.447-1.128-.344-3.805-.344-5.052s-.1-3.928.344-5.052A2.892,2.892,0,0,1,3.516,4.126c1.128-.447,3.805-.344,5.052-.344s3.928-.1,5.052.344A2.892,2.892,0,0,1,15.25,5.755c.447,1.128.344,3.805.344,5.052S15.7,14.735,15.25,15.86Z"
+								transform="translate(0.006 -2.238)"
+								fill="#fff"
+							/>
+						</svg>
+
+						<p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-[14px] xl:text-[16px] font-anton xl:leading-[16px]">
+							Share
+						</p>
+					</div>
+					<Link
+						href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+							`https://champagne-topaz.vercel.app?shared=${shareSchool}`
+						)}&text=${encodeURIComponent(diceOptions[diceIndex])}`}
+						className="bg-black text-white col-span-2 rounded-full flex flex-row space-x-2 items-center justify-center py-1 px-2 lg:py-[11px] xl:py-[12px] lg:px-[15px] 2xl:py-[17px] text-base font-black"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15.013"
+							height="12.194"
+							viewBox="0 0 15.013 12.194"
+						>
+							<path
+								id="Icon_awesome-twitter"
+								data-name="Icon awesome-twitter"
+								d="M13.47,6.42c.01.133.01.267.01.4a8.7,8.7,0,0,1-8.755,8.755A8.7,8.7,0,0,1,0,14.193a6.365,6.365,0,0,0,.743.038,6.162,6.162,0,0,0,3.82-1.315,3.082,3.082,0,0,1-2.877-2.134,3.88,3.88,0,0,0,.581.048,3.254,3.254,0,0,0,.81-.1A3.077,3.077,0,0,1,.61,7.706V7.668A3.1,3.1,0,0,0,2,8.058a3.082,3.082,0,0,1-.953-4.115,8.746,8.746,0,0,0,6.344,3.22,3.473,3.473,0,0,1-.076-.7,3.08,3.08,0,0,1,5.325-2.105,6.058,6.058,0,0,0,1.953-.743,3.069,3.069,0,0,1-1.353,1.7,6.168,6.168,0,0,0,1.772-.476A6.614,6.614,0,0,1,13.47,6.42Z"
+								transform="translate(0 -3.381)"
+								fill="#fff"
+							/>
+						</svg>
+
+						<p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-[14px] xl:text-[16px] font-anton xl:leading-[16px]">
+							Share
+						</p>
+					</Link>
+					<Link
+						href={`sms:&body=${
+							diceOptions[diceIndex]
+						}+${encodeURIComponent(
+							`https://champagne-topaz.vercel.app/share/${encodeURIComponent(
+								formData["College Name"] || ""
+							)}`
+						)}`}
+						className="bg-black text-white col-span-3 rounded-full flex flex-row space-x-2 items-center justify-center py-1 px-2 lg:py-[11px] xl:py-[12px] lg:px-[15px] 2xl:py-[17px] text-base font-black"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="21.203"
+							height="21.203"
+							viewBox="0 0 21.203 21.203"
+							className="w-[15px] h-[15px] lg:w-[21px] lg:h-[21px]"
+						>
+							<path
+								id="Icon_feather-message-square"
+								data-name="Icon feather-message-square"
+								d="M22.7,16.635a2.023,2.023,0,0,1-2.023,2.023H8.545L4.5,22.7V6.523A2.023,2.023,0,0,1,6.523,4.5H20.68A2.023,2.023,0,0,1,22.7,6.523Z"
+								transform="translate(-3 -3)"
+								fill="none"
+								stroke="#fff"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								strokeWidth="3"
+							/>
+						</svg>
+
+						<p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-[14px] xl:text-[16px] font-anton xl:leading-[16px] truncate">
+							Share with friends
+						</p>
+					</Link>
+					<Link
+						href={`sms:&body=${
+							diceOptions[diceIndex]
+						}+${encodeURIComponent(
+							`https://champagne-topaz.vercel.app/share/${encodeURIComponent(
+								formData["College Name"] || ""
+							)}`
+						)}`}
+						className="bg-black col-span-full text-white rounded-full flex flex-row space-x-2 items-center justify-center py-1 px-2 lg:py-[11px] xl:py-[12px] lg:px-[15px] 2xl:py-[17px] text-base font-black"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16.417"
+							height="18.192"
+							viewBox="0 0 16.417 18.192"
+						>
+							<path
+								id="Icon_simple-discord"
+								data-name="Icon simple-discord"
+								d="M16.2,0a1.994,1.994,0,0,1,1.976,1.876V18.192l-2.029-1.723-1.114-1.014-1.216-1.06.508,1.671H3.68a1.881,1.881,0,0,1-1.925-1.877V1.88A1.919,1.919,0,0,1,3.684,0H16.191ZM11.559,4.308h-.023l-.153.152a6.106,6.106,0,0,1,2.332,1.165,8.245,8.245,0,0,0-2.838-.862A7.793,7.793,0,0,0,9,4.762H8.849a6.758,6.758,0,0,0-2.13.557c-.354.154-.557.255-.557.255A5.408,5.408,0,0,1,8.6,4.409l-.1-.1a4.555,4.555,0,0,0-2.636.963,11.863,11.863,0,0,0-1.368,5.321,3.308,3.308,0,0,0,2.837,1.369s.3-.4.61-.76a3,3,0,0,1-1.622-1.064,2.514,2.514,0,0,1,.254.152h.045a.06.06,0,0,1,.045.023v0a.058.058,0,0,0,.045.023c.25.1.5.2.7.3a6.2,6.2,0,0,0,1.364.406,7.785,7.785,0,0,0,2.433,0,7.005,7.005,0,0,0,1.364-.406,10.934,10.934,0,0,0,1.059-.559,3.012,3.012,0,0,1-1.671,1.064c.25.353.6.758.6.758A3.956,3.956,0,0,0,15.5,10.6a11.839,11.839,0,0,0-1.376-5.321,4.559,4.559,0,0,0-2.6-.955l.042-.015Zm.127,3.345a1.015,1.015,0,1,1-.963,1.016.989.989,0,0,1,.963-1.016Zm-3.444,0a1.015,1.015,0,1,1-.966,1.016.988.988,0,0,1,.966-1.016Z"
+								transform="translate(-1.755)"
+								fill="#fff"
+							/>
+						</svg>
+
+						<p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-[14px] xl:text-[16px] font-anton xl:leading-[16px] truncate">
+							Join Discord
+						</p>
+					</Link>
+				</div>
+
+				{/* <div className="flex flex-row gap-x-2 w-full xl:max-w-[90%] 2xl:max-w-[97%] justify-between lg:gap-x-3 items-stretch">
 					<button
 						className="cursor-pointer bg-black text-white rounded-full flex flex-row space-x-2 items-center justify-center py-2 px-2.5 lg:py-3 lg:px-6  text-base font-black snapchat-share-button"
 						data-share-url={encodeURIComponent(
@@ -557,7 +674,7 @@ const SuccessForm = () => {
 							Share with friends
 						</p>
 					</Link>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
@@ -610,9 +727,9 @@ export default function Modal() {
 					>
 						<div
 							className={`
-									py-5 my-8 inline-block w-full max-w-[876px] xl:max-w-[933px] xl1:max-w-5xl 2xl:max-w-screen-xl transform overflow-hidden 
+									my-8 inline-block w-full lg1:max-w-4xl xl:max-w-5xl 2xl:max-w-[1355px] 3xl:max-w-[1806px] transform overflow-hidden 
 									rounded-[29px] xl:rounded-[53px] bg-gradient-to-br from-champagne-pink via-champagne-light-blue to-champagne-green 
-									text-left align-middle shadow-xl transition-all relative px-20
+									text-left align-middle shadow-xl transition-all relative max-w-7xl px-5 lg1:px-[69px] xl:px-[74px] xl1:px-[78px] 2xl:px-[104px]
 								`}
 						>
 							<span
@@ -654,7 +771,7 @@ export default function Modal() {
 								</svg>
 							</span>
 
-							{reqSuccessful ? <SuccessForm /> : <DataForm />}
+							{!reqSuccessful ? <SuccessForm /> : <DataForm />}
 						</div>
 					</Transition.Child>
 				</div>
