f71d086481264338fb8bdfe81643ed5386f3d753
Joshua Akangah <akangah89@gmail.com>
2022-12-05 21:49:47 +0000

    update

--- components/ambassador/ChampionsDo.tsx
+++ components/ambassador/ChampionsDo.tsx
@@ -9,30 +9,231 @@ export default function ChampionsDo() {
 						What Do Champions Do?
 					</h1>
 				</div>
-				<div className="mx-auto w-full flex flex-row space-x-[80px] items-center justify-center">
-					<div className="w-[1104px] h-[682px] shrink-0 relative">
-						<Image
-							className="object-cover"
-							fill
-							src="/images/content/champions-do.png"
-							alt="img"
-						/>
+				<div className="flex flex-col">
+					<div className="mx-auto w-full flex flex-row space-x-[80px] items-center justify-center">
+						<div className="w-[1104px] h-[682px] shrink-0 relative">
+							<Image
+								className="object-cover"
+								fill
+								src="/images/content/champions-do.png"
+								alt="img"
+							/>
+						</div>
+						<div className="w-[1104px] h-[682px] shrink-0 relative">
+							<Image
+								className="object-cover"
+								fill
+								src="/images/content/champions-do.png"
+								alt="img"
+							/>
+						</div>
+						<div className="w-[1104px] h-[682px] shrink-0 relative">
+							<Image
+								className="object-cover"
+								fill
+								src="/images/content/champions-do.png"
+								alt="img"
+							/>
+						</div>
 					</div>
-					<div className="w-[1104px] h-[682px] shrink-0 relative">
-						<Image
-							className="object-cover"
-							fill
-							src="/images/content/champions-do.png"
-							alt="img"
-						/>
-					</div>
-					<div className="w-[1104px] h-[682px] shrink-0 relative">
-						<Image
-							className="object-cover"
-							fill
-							src="/images/content/champions-do.png"
-							alt="img"
-						/>
+
+					<div className="flex flex-row items-center mx-auto max-w-[458px] space-x-[28px] mt-[62px]">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="51.766"
+							height="78.251"
+							viewBox="0 0 51.766 78.251"
+						>
+							<path
+								id="Path_14573"
+								data-name="Path 14573"
+								d="M0,0S30.092,24.352,30.092,26.139c0,.67-4.232,4.6-9.722,9.435C11.755,43.156,0,52.919,0,52.919"
+								transform="translate(39.092 65.593) rotate(180)"
+								fill="none"
+								stroke="#000"
+								stroke-linecap="round"
+								stroke-width="18"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="102.062"
+							height="13"
+							viewBox="0 0 102.062 13"
+						>
+							<path
+								id="Path_14574"
+								data-name="Path 14574"
+								d="M-3660.068,907.839h89.062"
+								transform="translate(3666.568 -901.339)"
+								fill="#c1fdfe"
+								stroke="#000"
+								stroke-linecap="round"
+								stroke-width="13"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.253"
+							height="17.254"
+							viewBox="0 0 17.253 17.254"
+						>
+							<g
+								id="Ellipse_1323"
+								data-name="Ellipse 1323"
+								fill="#d5d5d5"
+								stroke="#d5d5d5"
+								stroke-width="1"
+							>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.627"
+									ry="8.627"
+									stroke="none"
+								/>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.127"
+									ry="8.127"
+									fill="none"
+								/>
+							</g>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.253"
+							height="17.254"
+							viewBox="0 0 17.253 17.254"
+						>
+							<g
+								id="Ellipse_1323"
+								data-name="Ellipse 1323"
+								fill="#d5d5d5"
+								stroke="#d5d5d5"
+								stroke-width="1"
+							>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.627"
+									ry="8.627"
+									stroke="none"
+								/>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.127"
+									ry="8.127"
+									fill="none"
+								/>
+							</g>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.253"
+							height="17.254"
+							viewBox="0 0 17.253 17.254"
+						>
+							<g
+								id="Ellipse_1323"
+								data-name="Ellipse 1323"
+								fill="#d5d5d5"
+								stroke="#d5d5d5"
+								stroke-width="1"
+							>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.627"
+									ry="8.627"
+									stroke="none"
+								/>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.127"
+									ry="8.127"
+									fill="none"
+								/>
+							</g>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.253"
+							height="17.254"
+							viewBox="0 0 17.253 17.254"
+						>
+							<g
+								id="Ellipse_1323"
+								data-name="Ellipse 1323"
+								fill="#d5d5d5"
+								stroke="#d5d5d5"
+								stroke-width="1"
+							>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.627"
+									ry="8.627"
+									stroke="none"
+								/>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.127"
+									ry="8.127"
+									fill="none"
+								/>
+							</g>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17.253"
+							height="17.254"
+							viewBox="0 0 17.253 17.254"
+						>
+							<g
+								id="Ellipse_1323"
+								data-name="Ellipse 1323"
+								fill="#d5d5d5"
+								stroke="#d5d5d5"
+								stroke-width="1"
+							>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.627"
+									ry="8.627"
+									stroke="none"
+								/>
+								<ellipse
+									cx="8.627"
+									cy="8.627"
+									rx="8.127"
+									ry="8.127"
+									fill="none"
+								/>
+							</g>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="51.765"
+							height="78.251"
+							viewBox="0 0 51.765 78.251"
+						>
+							<path
+								id="Path_14572"
+								data-name="Path 14572"
+								d="M-3597.4,888.2s30.092,24.352,30.092,26.139-30.092,26.78-30.092,26.78"
+								transform="translate(3610.073 -875.54)"
+								fill="none"
+								stroke="#000"
+								stroke-linecap="round"
+								stroke-width="18"
+							/>
+						</svg>
 					</div>
 				</div>
 			</div>
